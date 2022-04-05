89d88a1bf05615c8dc1584c9a3ccb3f7e37c40fb
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2022-04-05 15:18:13 +0545

    Databse handlers updated

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 
 int main(int argc, char *argv[])
 {
-    QSqlDatabase db;
-    db = QSqlDatabase::addDatabase("QSQLITE"); // adding the sqlite engine
-    db.setDatabaseName("mydb.sqlite");
-    run_database(db);
+//    qputenv("QT_DEBUG_PLUGINS", QByteArray("1"));
+//    QSqlDatabase *db = new  QSqlDatabase;
+//    *db = QSqlDatabase::addDatabase("QSQLITE"); // adding the sqlite engine
+//    db->setDatabaseName("planner.sqlite");
+//    run_database(*db);
     QApplication a(argc, argv);
     MainWindow w;
     w.showMaximized();
